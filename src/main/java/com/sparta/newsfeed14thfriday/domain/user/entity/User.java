51a4ebbc0b1a42de69db2848987f564a0e284982
multiverse22 <momizi527@gmail.com>
2024-09-02 21:08:13 +0900

    User와 friend엔티티 생성

--- src/main/java/com/sparta/newsfeed14thfriday/domain/user/entity/User.java
+++ src/main/java/com/sparta/newsfeed14thfriday/domain/user/entity/User.java
@@ -1,22 +1,24 @@
 package com.sparta.newsfeed14thfriday.domain.user.entity;
 
+import com.sparta.newsfeed14thfriday.domain.friend.entity.Friend;
 import com.sparta.newsfeed14thfriday.entity_common.Timestamped;
 import jakarta.persistence.*;
 import lombok.Getter;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
 @Entity
-public class Users extends Timestamped {
+public class User extends Timestamped {
+
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
+    @Column(nullable = false,length = 50,unique = true)
+    private String email;
 
     @Column(nullable = false,length = 50)
     private String username;
-
-    @Column(nullable = false,length = 50)
-    private String email;
 
     @Column(nullable = false,length = 200)
     private String password;
@@ -25,4 +27,7 @@ public class Users extends Timestamped {
     private boolean deleted = false;
 
     private Long friendsCount;
+
+    @OneToMany(mappedBy = "UsersFriend")
+    private List<Friend> friends = new ArrayList<>();
 }
