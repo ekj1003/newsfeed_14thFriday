dd072784ba3ea69cc77276e945d86de823a14a9b
multiverse22 <momizi527@gmail.com>
2024-09-02 19:48:35 +0900

    init commit

---
+++ src/test/java/com/sparta/newsfeed14thfriday/Newsfeed14thfridayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.newsfeed14thfriday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Newsfeed14thfridayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
