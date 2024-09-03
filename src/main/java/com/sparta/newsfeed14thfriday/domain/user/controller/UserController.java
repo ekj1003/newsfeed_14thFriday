729415aca802dfd713d142c85f24cf173fecc237
Eunkyo <jeongeunkyo3@gmail.com>
2024-09-03 17:34:28 +0900

    ♻️refactor : jwt 회원가입, 로그인 auth로 다시 구현

--- src/main/java/com/sparta/newsfeed14thfriday/domain/user/controller/UserController.java
+++ src/main/java/com/sparta/newsfeed14thfriday/domain/user/controller/UserController.java
@@ -1,13 +1,6 @@
 package com.sparta.newsfeed14thfriday.domain.user.controller;
 
-import com.sparta.newsfeed14thfriday.domain.user.dto.LoginRequestDto;
-import com.sparta.newsfeed14thfriday.domain.user.dto.SignupRequestDto;
-import com.sparta.newsfeed14thfriday.domain.user.dto.SignupResponseDto;
 import com.sparta.newsfeed14thfriday.domain.user.service.UserService;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
@@ -18,23 +11,6 @@ public class UserController {
 
     public UserController(UserService userService) {
         this.userService = userService;
-    }
-    // 회원 가입
-    @PostMapping("/signup")
-    public SignupResponseDto signup(@RequestBody SignupRequestDto requestDto) {
-        return userService.signup(requestDto);
-    }
-
-    // 로그인
-    @PostMapping("/login")
-    public String login(@RequestBody LoginRequestDto requestDto, HttpServletResponse res) {
-        try {
-            userService.login(requestDto, res);
-        } catch (Exception e) {
-            return "아이디 또는 비밀번호가 잘못 되었습니다. 아이디와 비밀번호를 정확히 입력해 주세요.";
-        }
-
-        return "로그인에 성공했습니다. 환영합니다!";
     }
 
 
--- src/main/java/com/sparta/newsfeed14thfriday/domain/user/service/UserService.java
+++ src/main/java/com/sparta/newsfeed14thfriday/domain/user/service/UserService.java
@@ -1,19 +1,10 @@
 package com.sparta.newsfeed14thfriday.domain.user.service;
 
-import com.sparta.newsfeed14thfriday.domain.user.dto.LoginRequestDto;
-import com.sparta.newsfeed14thfriday.domain.user.dto.SignupRequestDto;
-import com.sparta.newsfeed14thfriday.domain.user.dto.SignupResponseDto;
-import com.sparta.newsfeed14thfriday.domain.user.entity.User;
 import com.sparta.newsfeed14thfriday.domain.user.repository.UserRepository;
 import com.sparta.newsfeed14thfriday.global.config.PasswordEncoder;
 import com.sparta.newsfeed14thfriday.global.jwt.JwtUtil;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
-@Slf4j
 @Service
 public class UserService {
 
@@ -25,53 +16,6 @@ public class UserService {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
         this.jwtUtil = jwtUtil;
-    }
-
-    // 회원 가입 구현
-    public SignupResponseDto signup(SignupRequestDto requestDto) {
-        String username = requestDto.getUsername(); // username 받아오기
-
-        // 이메일 형식 확인
-        if (!requestDto.isEmailValid()) {
-            throw new IllegalArgumentException("이메일 형식이 올바르지 않습니다.");
-        }
-
-        // 비밀번호 형식 확인
-        if (!requestDto.isPasswordValid()) {
-            throw new IllegalArgumentException("비밀번호는 최소 8자 이상이어야 하며, 대소문자 포함 영문, 숫자, 특수문자를 최소 1글자씩 포함해야 합니다.");
-        }
-
-        String password = passwordEncoder.encode(requestDto.getPassword()); // 받아온 password 암호화해서 넣기
-
-        // email 중복확인, email: unique=true
-        String email = requestDto.getEmail();
-        Optional<User> checkEmail = userRepository.findByEmail(email);
-        if (checkEmail.isPresent()) {
-            throw new IllegalArgumentException("중복된 Email 입니다.");
-        }
-
-        User user = new User(username, password, email);
-        userRepository.save(user);
-        return new SignupResponseDto(user);
-    }
-
-    public void login(LoginRequestDto requestDto, HttpServletResponse res) {
-        String email = requestDto.getEmail();
-        String password = requestDto.getPassword();
-
-        // 사용자 확인
-        User user = userRepository.findByEmail(email).orElseThrow(
-                () -> new IllegalArgumentException("등록된 사용자가 없습니다.")
-        );
-
-        // 비밀번호 확인
-        if (!passwordEncoder.matches(password, user.getPassword())) {
-            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
-        }
-
-        // JWT 생성 및 쿠키에 저장 후 Response 객체에 추가
-        String token = jwtUtil.createToken(user.getEmail());
-        jwtUtil.addJwtToCookie(token, res);
     }
 
 }
