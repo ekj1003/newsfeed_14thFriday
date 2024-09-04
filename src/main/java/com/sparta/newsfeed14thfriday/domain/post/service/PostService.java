40d40c3074d60a64408bdfae60d492347c72759f
Hyemin Yang <asitwas729@gmail.com>
2024-09-04 11:34:23 +0900

    Post ?

--- src/main/java/com/sparta/newsfeed14thfriday/domain/post/service/PostService.java
+++ src/main/java/com/sparta/newsfeed14thfriday/domain/post/service/PostService.java
@@ -86,18 +86,20 @@ public class PostService {
     }
 
     @Transactional
-    public void deletePost(Long postId) {
+    public void deletePost(Long postId, String email) {
         // 조회: postId, userId
         Post post = postRepository.findByPostId(postId).orElseThrow(() -> new NullPointerException("Post not found."));
-//        User user = userRepository.findById(data.getUserId()).orElseThrow(() -> new NullPointerException("User not found."));
-//
-//        //작성자 일치 여부
-//        if(post.getUser() == null || !ObjectUtils.nullSafeEquals(user.getId(), post.getUser.getId())){
-//            throw new IllegalArgumentException("작성자가 일치하지않습니다.");
-//        } else{
-//            // delete
-//            postRepository.deleteByPostId(postId)
-//        }
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new NullPointerException("User not found."));
+
+        //작성자 일치 여부
+        if(post.getUser() == null || !ObjectUtils.nullSafeEquals(user.getEmail(), post.getUser().getEmail())){
+            throw new IllegalArgumentException("작성자가 일치하지않습니다.");
+        } else{
+            // delete
+            postRepository.deleteByPostId(postId);
+        }
+        
+        // 삭제
         postRepository.deleteByPostId(postId);
     }
 
