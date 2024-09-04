8f0213775659af492c3582eab2ab7983d0aae76a
EtherXion <etel0103@gmail.com>
2024-09-04 20:56:56 +0900

    controller 부분 정리

--- src/main/java/com/sparta/newsfeed14thfriday/domain/comment/controller/CommentController.java
+++ src/main/java/com/sparta/newsfeed14thfriday/domain/comment/controller/CommentController.java
@@ -22,13 +22,13 @@ public class CommentController {
     }
 
     // 어느 포스트에 코멘트를 작성할건지 API 변경 필요
-    @PostMapping("/comments/{postId}")
+    @PostMapping("/posts/{postId}/comments")
     public CommentSaveResponseDto createComment(@PathVariable Long postId, @RequestBody CommentSaveRequestDto commentSaveRequestDto) {
         return commentService.createComment(postId, commentSaveRequestDto);
     }
 
     // 어느 포스트의 코멘트를 조회할건지 전체 조회 기능보다 이게 나을 듯?
-    @GetMapping("/comments/{postId}")
+    @GetMapping("/posts/{postId}/comments")
     public List<CommentDetailResponseDto> getComments(@PathVariable Long postId) {
         return commentService.getComments(postId);
     }
@@ -44,7 +44,7 @@ public class CommentController {
 //        commentService.deleteComment(commentId);
 //    } // 아예 삭제하는 버전
 
-    @PutMapping("/comments/delete/{commentId}")
+    @PutMapping("/comments/{commentId}/delete")
     public void deleteCommentUpdate(@PathVariable Long commentId, @RequestBody CommentDeleteRequestDto commentDeleteRequestDto) {
         commentService.deleteCommentUpdate(commentId, commentDeleteRequestDto);
     }
